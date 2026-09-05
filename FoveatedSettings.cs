1e0b88beffbb323931a670d34bc8cfc86363f46f
Arthur Pinna <arthurpinnaemerim@gmail.com>
2026-09-05 06:42:07 -0300

    Add files via upload

--- FoveatedSettings.cs
+++ FoveatedSettings.cs
@@ -15,18 +15,22 @@ public class FoveationSettings : MonoBehaviour
 {
   public float FoveateLevel = 1.0f;
 [SerializeField] private UnityEngine.XR.Interaction.Toolkit.Interactors.XRGazeInteractor gazeInteractor;
-
+private DepthOfField depthOfField;
+public Transform cameraTransform;
 private Thread receiveThread;
+public Volume volume;
+private Vignette vignette;
+private Bloom bloom;
+private MotionBlur motionBlur;
+[SerializeField] private UniversalRendererData rendererData;
+private ColorAdjustments colorAdjustments;
 private UdpClient client;
+[SerializeField]public Light mainLight; 
 public int port = 8005;
 private string lastReceivedMessage = "";
 private bool hasNewMessage = false;
-
 private volatile bool isRunning = false;
-
 float gazetrack = 1;
-
-
   private List<XRDisplaySubsystem> xrDisplays = new List<XRDisplaySubsystem>();
 
   void Start()
@@ -43,6 +47,31 @@ float gazetrack = 1;
     receiveThread.IsBackground = true;
     receiveThread.Start();
     Debug.Log($"Server started, listening on port {port}...");
+    EnsureColorAdjustmentsReference();
+    if (volume.profile.TryGet<DepthOfField>(out depthOfField))
+        {
+            depthOfField.focusDistance.overrideState = true;
+        }
+        else
+        {
+            depthOfField = volume.profile.Add<DepthOfField>(true);
+        }
+    if (volume.profile.TryGet<Vignette>(out vignette))
+        {
+            vignette.intensity.overrideState = true;
+        }
+        else
+        {
+            vignette = volume.profile.Add<Vignette>(true);
+        }
+    if (!volume.profile.TryGet<MotionBlur>(out motionBlur))
+            {
+                Debug.LogWarning("MotionBlur component not found on the Volume Profile!");
+            }
+    if (volume.profile.TryGet<Bloom>(out bloom))
+        {
+            bloom.intensity.overrideState = true;
+        }
   }
     void Update()
     {
@@ -63,7 +92,30 @@ float gazetrack = 1;
             }
         }
     }
+    private void EnsureColorAdjustmentsReference()
+        {
+            if (colorAdjustments != null) return; // Already initialized
 
+        if (volume == null)
+        {
+            // Try to find a Volume on the same GameObject if unassigned
+            volume = GetComponent<Volume>();
+        }
+
+        if (volume != null && volume.profile != null)
+        {
+            // Fetch or create ColorAdjustments override
+            if (!volume.profile.TryGet(out colorAdjustments))
+            {
+                colorAdjustments = volume.profile.Add<ColorAdjustments>(true);
+                Debug.Log("ColorAdjustments was missing, so it was added to the profile automatically.");
+            }
+        }
+        else
+        {
+            Debug.LogError("Volume or Volume Profile is missing on " + gameObject.name, this);
+        }
+    }
     private void StartListener()
     {
         try
@@ -86,7 +138,7 @@ float gazetrack = 1;
         }
     }
 
-private void ReceiveData()
+    private void ReceiveData()
     {
         while (isRunning)
         {
@@ -130,6 +182,16 @@ private void ReceiveData()
         }
     }
 
+    void updatecolorcorrection(float satur, float contra, float expo)
+    {
+        if (colorAdjustments != null){
+        colorAdjustments.saturation.overrideState = true;
+        colorAdjustments.saturation.Override(satur);
+        colorAdjustments.contrast.Override(contra);
+        colorAdjustments.postExposure.Override(expo);
+        }
+    }
+
     void AdjustRenderScale(float renderscale)
     {
         renderscale = Mathf.Clamp(renderscale, 0.1f, 2.0f);
@@ -146,38 +208,230 @@ private void ReceiveData()
         int resoy = (int)resolutiony;
         Screen.SetResolution(resox, resoy, true);
     }
-
+    void AdjustShadows(string force, float distance)
+    {
+        var urpAsset = GraphicsSettings.currentRenderPipeline as UniversalRenderPipelineAsset;
+        urpAsset.shadowDistance = distance;
+        Debug.Log(force);
+        if(force == "hard")
+        {
+            Debug.Log("hard");
+            QualitySettings.shadows = UnityEngine.ShadowQuality.HardOnly;
+        }
+        else if(force == "off")
+        {
+            Debug.Log("off");
+            urpAsset.shadowDistance = 0;
+        }
+        else
+        {
+            Debug.Log("all");
+            QualitySettings.shadows = UnityEngine.ShadowQuality.All;
+        }
+    }
+    void AdjustBloom(float active, float bloompower)
+    {
+        if(active == 0)
+        {
+            Debug.Log("Bloom off");
+            bloom.active = false;
+        }
+        else
+        {
+            Debug.Log("bloom on");
+            Debug.Log(bloompower);
+            bloom.active = true;
+            bloom.intensity.value = bloompower;
+        }
+    }
+    void AdjustSSAO(float active)
+    {
+        if (rendererData == null){ Debug.Log("no render data") ;return;}
+        foreach (var feature in rendererData.rendererFeatures)
+        {
+            if (feature is ScreenSpaceAmbientOcclusion ssao)
+            {
+                if (active == 0)
+                {
+                    Debug.Log("SSao off");
+                    ssao.SetActive(false);
+                }
+                else
+                {
+                    Debug.Log("SSao on");
+                    ssao.SetActive(true);
+                }
+                break;
+            }
+        }
+    }
+    void AdjustFog(float active, float fogpower)
+    {
+        if(active == 0)
+        {
+            RenderSettings.fog = false;
+        }
+        else
+        {
+            RenderSettings.fog = true;
+            fogpower = fogpower / 100;
+            Debug.Log(fogpower);
+            RenderSettings.fogDensity = fogpower;
+        }
+    }
     private void ProcessNetworkMessage(string message)
     {
         string command = message.Trim();
         Debug.Log($"Received controller command: {command}");
         string[] parts = command.Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
-        if (parts.Length >= 3)
-            {
-            if (float.TryParse(parts[0], out float foveateLevel) && float.TryParse(parts[2], out float foveateEnable))
+        //parts[0] calls function. 0 = foveation. 1 = GazeTrack. 2=scalerender, 3=resolution, etc....
+        if (parts[0] == "0" && (parts.Length >= 3)){
+            if (float.TryParse(parts[2], out float foveateLevel) && float.TryParse(parts[1], out float foveateEnable))
             {
                 AdjustFoveation(foveateEnable, foveateLevel);
             }
-
+        }
+        else if(parts[0] == "1" && (parts.Length >= 2)){
             if (float.TryParse(parts[1], out float gazeTrack))
             {
                 gazetrack = gazeTrack;
                 SetGazeAllowed(gazetrack);
             }
-            if (float.TryParse(parts[3], out float scalerender))
+        }
+        else if(parts[0] == "2" && (parts.Length >= 2)){
+            if (float.TryParse(parts[1], out float scalerender))
             {
                 AdjustRenderScale(scalerender);
             }
-            if(float.TryParse(parts[4], out float resx) && float.TryParse(parts[5], out float resy))
+        }
+        else if(parts[0] == "3" && (parts.Length >= 3)){
+            if(float.TryParse(parts[1], out float resx) && float.TryParse(parts[2], out float resy))
             {
                 AdjustResolution(resx, resy);
             }
         }
+        else if(parts[0] == "4" && (parts.Length >= 3))
+        {
+            if(float.TryParse(parts[1], out float antialias) && int.TryParse(parts[2], out int aliasval))
+            {
+                if(antialias == 0)
+                {
+                    QualitySettings.antiAliasing = 0;
+                }
+                else
+                {
+                    QualitySettings.antiAliasing = aliasval;
+                }
+            }
+        }
+        else if(parts[0] == "5" && (parts.Length >= 2))
+        {
+            if (int.TryParse(parts[1], out int vsyncon))
+            {
+                QualitySettings.vSyncCount = vsyncon;
+            }
+        }
+        else if(parts[0] == "6" && (parts.Length >= 2) && mainLight != null)
+        {
+            if (float.TryParse(parts[1], out float mainintensity))
+            {
+                mainLight.intensity = mainintensity;
+            }
+        }
+        else if(parts[0] == "7")
+        {
+            if (float.TryParse(parts[1], out float envintensity))
+            {
+                RenderSettings.ambientIntensity = envintensity;
+                DynamicGI.UpdateEnvironment();
+            }
+        }
+        else if(parts[0] == "8")
+        {
+            if (float.TryParse(parts[1], out float saturationvalue) && float.TryParse(parts[2], out float contrastvalue) && float.TryParse(parts[3], out float expositionvalue))
+            {
+                updatecolorcorrection(saturationvalue, contrastvalue, expositionvalue);
+            }
+        }
+        else if(parts[0] == "9")
+        {
+            if(float.TryParse(parts[1], out float farclip))
+            {
+                Debug.Log("fortnite battle pass");
+                GetComponentInChildren<Camera>().farClipPlane = farclip;
+            }
+        }
+        else if(parts[0] == "10")
+        {
+            if(float.TryParse(parts[1], out float shadistance))
+            {
+                AdjustShadows(parts[2], shadistance);
+            }
+        }
+        else if(parts[0] == "11" && (parts.Length >= 3)){
+            if (float.TryParse(parts[2], out float bloomLevel) && float.TryParse(parts[1], out float bloomEnable))
+            {
+                AdjustBloom(bloomEnable, bloomLevel);
+            }
+        }
+        else if(parts[0] == "12")
+        {
+            if(int.TryParse(parts[1], out int motionon))
+            {
+                if(motionon == 1){
+                    Debug.Log("blur on");
+                    motionBlur.active = true;
+                }
+                else
+                {
+                    Debug.Log("blur off");
+                    motionBlur.active = false;
+                }
+            }
+        }
+        else if(parts[0] == "13")
+        {
+            if (float.TryParse(parts[1], out float ssaoEnable))
+            {
+                AdjustSSAO(ssaoEnable);
+            }            
+        }
+        else if(parts[0] == "14")
+        {
+            {   
+            if (depthOfField == null || cameraTransform == null) 
+            {
+              Debug.LogWarning("DepthOfField or cameraTransform is missing!");
+                return;
+            }
+         if (float.TryParse(parts[1], System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out float focusdist))
+            {
+                depthOfField.focusDistance.overrideState = true;
+                depthOfField.focusDistance.value = focusdist;
+            }
+        }
+        }
+        else if(parts[0] == "15")
+        {
+            if(vignette != null && float.TryParse(parts[1], out float vignvalue))
+            {
+                vignvalue = vignvalue / 1000;
+                Debug.Log(vignvalue);
+                vignette.intensity.value = vignvalue;
+            }
+        }
+        else if(parts[0] == "16")
+        {
+            if(float.TryParse(parts[1], out float fogactive) && float.TryParse(parts[2], out float fogpower))
+            {
+                AdjustFog(fogactive, fogpower);
+            }
+        }
         else
         {
-            Debug.LogWarning($"Malformed network message. Expected 3 values, got: {parts.Length}");
+            Debug.LogWarning($"Invalid message received");
         }
-        }  
+    }
     public void SetGazeAllowed(float state)
     {
         SubsystemManager.GetSubsystems(xrDisplays);
--- PythonSlider.py
+++ PythonSlider.py
@@ -7,48 +7,34 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 root = Tk()
 root.title("TKinter Slider")
-root.geometry("400x400")
+root.geometry("900x400")
 
 value = 0
 is_on = True
 fov_is_on = True
+bloom_is_on = True
+fog_is_on = False
+ssao_is_on = True
 resx = 1080
 resy = 1080
+antialias_on = False
+vsync_on = False
+motion_on = False
 
-
-
-def send_data_to_unity():
-    """Helper function to grab current states, bundle them, and send via UDP"""
-    # 1. Get the current slider value
-    current_value = float(slider.get()) / 100
-    render_scale = float(renderslider.get()) / 10
+def send_udp_message(message):
     try:
-        resx = float(ResoXbox.get())
-    except ValueError:
-        resx= 1080
-    try:
-        resy = float(ResoYbox.get())
-    except ValueError:
-        resy= 1080
-    state_int = 1 if is_on else 0
-    fov_int = 1 if fov_is_on else 0
-    
-    # 3. Format as a comma-separated string (e.g., "45.5,1")
-    message = f"{current_value},{state_int},{fov_int},{render_scale},{resx},{resy}".encode('utf-8')
-    
-    try:
-        sock.sendto(message, (UDP_IP, UDP_PORT))
+        encoded_message = message.encode('utf-8')
+        sock.sendto(encoded_message, (UDP_IP, UDP_PORT))
     except Exception as e:
         print(f"Failed to send data: {e}")
 
-def toggle():
-    global is_on
-    is_on = not is_on
-    if is_on:
-        toggle_btn.config(text='ON', bg='green', fg='white')
-    else:
-        toggle_btn.config(text='OFF', bg='red', fg='white')
-    send_data_to_unity()
+"FOVEATED RENDERING"
+
+def update_foveation(dump):
+    current_value = float(slider.get()) / 100
+    fov_int = 1 if fov_is_on else 0
+    message = f"0, {fov_int}, {current_value}"
+    send_udp_message(message)
 
 def toggle_fov():
     global fov_is_on
@@ -57,58 +43,292 @@ def toggle_fov():
         foveate_btn.config(text='ON', bg='green', fg='white')
     else:
         foveate_btn.config(text='OFF', bg='red', fg='white')
-    send_data_to_unity()
+    update_foveation(1)
 
-
-def update_unity(value):
-    send_data_to_unity() 
-
-"FOVEATED RENDERING"
-
-slider = Scale(root, from_=0.00, to=100, orient=HORIZONTAL, length= 150, command= update_unity)
-slider.pack(padx=20, side=LEFT)
+slider = Scale(root, from_=0.00, to=100, orient=HORIZONTAL, length= 150, command= update_foveation)
 slider.place(x=5, y=90)
-
 foveate_btn = Button(root, text='ON', bg='green', fg='white', font=('Arial', 16), command=toggle_fov, width=10)
-foveate_btn.pack(pady=30)
 foveate_btn.place(x=10, y=50)
-
 foveate_label = Label(root, text='Foveated Rendering', font=('Arial', 12) )
-foveate_label.pack()
 foveate_label.place(x=12, y=20)
 
 "GAZE TRACKING"
 
-toggle_btn = Button(root, text='ON', bg='green', fg='white', font=('Arial', 16), command=toggle, width=10)
-toggle_btn.pack(pady=20)
-toggle_btn.place(x=10, y=180)
+def toggle():
+    global is_on
+    is_on = not is_on
+    if is_on:
+        toggle_btn.config(text='ON', bg='green', fg='white')
+    else:
+        toggle_btn.config(text='OFF', bg='red', fg='white')
+    state_int = 1 if is_on else 0
+    message = f"1, {state_int}"
+    send_udp_message(message)
 
+toggle_btn = Button(root, text='ON', bg='green', fg='white', font=('Arial', 16), command=toggle, width=10)
+toggle_btn.place(x=10, y=180)
 status_label = Label(root, text='Gaze tracking', font=('Arial', 12))
-status_label.pack()
 status_label.place(x = 12, y=150)
 
 "RENDER SCALING"
 
-renderslider = Scale(root, from_=1, to=20, orient=HORIZONTAL, length= 150, command= update_unity)
-renderslider.pack(padx=20, side=LEFT)
+def update_render(dump):
+    render_scale = float(renderslider.get()) / 10
+    message = f"2, {render_scale}"
+    send_udp_message(message)
+
+renderslider = Scale(root, from_=1, to=20, orient=HORIZONTAL, length= 150, command= update_render)
 renderslider.place(x=5, y=250)
 renderlabel = Label(root, text='Render Scaling', font=('Arial', 12))
-renderlabel.pack()
 renderlabel.place(x = 12, y=230)
 
 "RESOLUTION"
+
+def update_resolution():
+    print("button")
+    try:
+        resx = float(ResoXbox.get())
+    except ValueError:
+        resx= 1080
+    try:
+        resy = float(ResoYbox.get())
+    except ValueError:
+        resy= 1080
+    message = f"3, {resx}, {resy}"
+    send_udp_message(message)
+
 resolabel = Label(root, text='Resolution', font=('Arial', 12))
-resolabel.pack()
 resolabel.place(x = 12, y=300)
 ResoXbox = Spinbox(root, from_=0, increment=100, width=5)
-ResoXbox.pack()
 ResoXbox.place(x=10, y=320)
 ResoYbox = Spinbox(root, from_=0, increment=100, width=5)
-ResoYbox.pack()
 ResoYbox.place(x=60, y=320)
-Resobtn = Button(root, text="Update", command=send_data_to_unity)
-Resobtn.pack()
+Resobtn = Button(root, text="Update", command=update_resolution)
 Resobtn.place(x=30, y=350)
 
+"ANTI-ALIASING"
+
+def update_aliasing():
+    antialias = 1 if antialias_on else 0
+    aliasvalue = antialiasbox.get()
+    message = f"4, {antialias}, {aliasvalue}"
+    send_udp_message(message)
+
+def toggle_antialias():
+    global antialias_on
+    antialias_on = not antialias_on
+    if antialias_on:
+        antialias_btn.config(text='ON', bg='green', fg='white')
+    else:
+        antialias_btn.config(text='OFF', bg='red', fg='white')
+    update_aliasing()
+
+antialias_btn = Button(root, text='OFF', bg='red', fg='white', font=('Arial', 16), command=toggle_antialias, width=10)
+antialias_btn.place(x=200, y=50)
+antialias_label = Label(root, text='Anti Aliasing', font=('Arial', 12))
+antialias_label.place(x = 212, y=20)
+antialiasbox = Spinbox(root, from_=2, to=8, increment=2, width=5)
+antialiasbox.place(x=200, y=110)
+aliasbtn = Button(root, text="Update", command=update_aliasing)
+aliasbtn.place(x=280, y=105)
+
+"V-SYNC"
+
+def updatevsync():
+    global vsync_on
+    vsync_on = not vsync_on
+    if vsync_on:
+        vsync_btn.config(text='ON', bg='green', fg='white')
+    else:
+        vsync_btn.config(text='OFF', bg='red', fg='white')
+    vsync_int = 1 if vsync_on else 0
+    message = f"5, {vsync_int}"
+    send_udp_message(message)
+
+vsync_btn = Button(root, text='OFF', bg='red', fg='white', font=('Arial', 16), command=updatevsync, width=10)
+vsync_btn.place(x=200, y=180)
+vsync_label = Label(root, text='V-SYNC', font=('Arial', 12))
+vsync_label.place(x = 212, y=150)
+
+"MAIN LIGHTING"
+def updatelight(intensity):
+    level = float(intensity)/10
+    message = f"6, {level}"
+    send_udp_message(message)
+lightslider = Scale(root, from_=0, to=80, orient=HORIZONTAL, length= 150, command= updatelight)
+lightslider.place(x=190, y=250)
+mainlight_label = Label(root, text='Main Lighting', font=('Arial', 12))
+mainlight_label.place(x = 210, y=230)
+
+"ENVIROMENT LIGHTING"
+def updateenvi(intensity):
+    level = float(intensity)/10
+    message = f"7, {level}"
+    send_udp_message(message)
+envislider = Scale(root, from_=0, to=80, orient=HORIZONTAL, length= 150, command= updateenvi)
+envislider.place(x=190, y=320)
+envilight_label = Label(root, text='Enviroment Lighting', font=('Arial', 12))
+envilight_label.place(x = 195, y=300)
+
+"COLOR CORRECTION"
+def updatecolor(dump):
+    saturation = float(saturslider.get())
+    contrast = float(contraslider.get())
+    exposition = float(exposlider.get())
+    message = f"8, {saturation}, {contrast}, {exposition}"
+    send_udp_message(message)
+saturslider = Scale(root, from_=-100, to=100, orient=HORIZONTAL, length= 150, command= updatecolor)
+saturslider.place(x=350, y=30)
+saturlabel = Label(root, text='Saturation', font=('Arial', 12))
+saturlabel.place(x = 387, y=10)
+
+contraslider = Scale(root, from_=-100, to=100, orient=HORIZONTAL, length= 150, command= updatecolor)
+contraslider.place(x=350, y=100)
+contralabel = Label(root, text='Contrast', font=('Arial', 12))
+contralabel.place(x = 390, y=80)
+
+exposlider = Scale(root, from_=-100, to=100, orient=HORIZONTAL, length= 150, command= updatecolor)
+exposlider.place(x=350, y=170)
+expolabel = Label(root, text='Exposure', font=('Arial', 12))
+expolabel.place(x = 387, y=150)
+
+"RENDER DISTANCE"
+def updatefarclip(dump):
+    farclip = (float(farclipslider.get()))
+    message = f"9, {farclip}"
+    send_udp_message(message)
+
+farclipslider = Scale(root, from_=1, to=1000, orient=HORIZONTAL, length= 150, command= updatefarclip)
+farclipslider.place(x=350, y=240)
+farcliplabel = Label(root, text='Render Distance', font=('Arial', 12))
+farcliplabel.place(x = 367, y=220)
+
+"SHADOW QUALITY"
+def updateshadows(dump):
+    print("button")
+    shadist = float(shadowslider.get())
+    shadval = currentshadow.get()
+    message = f"10, {shadist}, {shadval} "
+    send_udp_message(message)
+def redoshadows():
+    updateshadows(1)
+
+shadowlist = ["hard", "soft", "off"]
+currentshadow = StringVar(root)
+currentshadow.set(shadowlist[0])
+shadowslider = Scale(root, from_=0, to=500, orient=HORIZONTAL, length= 150, command= updateshadows)
+shadowslider.place(x=350, y=300)
+shadowlabel = Label(root, text='Shadow Distance', font=('Arial', 12))
+shadowlabel.place(x = 367, y=280)
+shadowselect = OptionMenu(root, currentshadow, *shadowlist)
+shadowselect.place(x = 350, y = 340)
+shadowbtn = Button(root, text="Update", command= redoshadows)
+shadowbtn.place(x=430, y=342)
+
+"BLOOM"
+def updatebloom(dump):
+    current_value = float(bloomslider.get())
+    bloom_int = 1 if bloom_is_on else 0
+    message = f"11, {bloom_int}, {current_value}"
+    send_udp_message(message)
+
+def togglebloom():
+    global bloom_is_on
+    bloom_is_on = not bloom_is_on
+    if bloom_is_on:
+        bloombtn.config(text='ON', bg='green', fg='white')
+    else:
+        bloombtn.config(text='OFF', bg='red', fg='white')
+    updatebloom(1)
+
+bloomslider = Scale(root, from_=0, to=100, orient=HORIZONTAL, length= 150, command= updatebloom)
+bloomslider.place(x=510, y=90)
+bloombtn = Button(root, text='ON', bg='green', fg='white', font=('Arial', 16), command=togglebloom, width=10)
+bloombtn.place(x=525, y=50)
+bloomlabel = Label(root, text='Bloom', font=('Arial', 12) )
+bloomlabel.place(x=560, y=20)
+
+"MOTION BLUR"
+
+def updatemotion():
+    global motion_on
+    motion_on = not motion_on
+    if motion_on:
+        motion_btn.config(text='ON', bg='green', fg='white')
+    else:
+        motion_btn.config(text='OFF', bg='red', fg='white')
+    motion_int = 1 if motion_on else 0
+    message = f"12, {motion_int}"
+    send_udp_message(message)
+
+motion_btn = Button(root, text='OFF', bg='red', fg='white', font=('Arial', 16), command=updatemotion, width=10)
+motion_btn.place(x=525, y=175)
+motion_label = Label(root, text='Motion Blur', font=('Arial', 12))
+motion_label.place(x = 545, y=145)
+
+"SSAO"
+
+def updatessao(dump):
+    ssao_int = 1 if ssao_is_on else 0
+    message = f"13, {ssao_int}"
+    send_udp_message(message)
+
+def togglessao():
+    global ssao_is_on
+    ssao_is_on = not ssao_is_on
+    if ssao_is_on:
+        ssaobtn.config(text='ON', bg='green', fg='white')
+    else:
+        ssaobtn.config(text='OFF', bg='red', fg='white')
+    updatessao(1)
+ssaobtn = Button(root, text='ON', bg='green', fg='white', font=('Arial', 16), command=togglessao, width=10)
+ssaobtn.place(x=525, y=255)
+ssaolabel = Label(root, text='SSAO', font=('Arial', 12) )
+ssaolabel.place(x=560, y=230)
+
+"DEPTH OF FIELD"
+def updatedepth(dump):
+    depthlevel = float(depthslider.get())
+    message = f"14, {depthlevel}"
+    send_udp_message(message)
+depthslider = Scale(root, from_=1, to=100, orient=HORIZONTAL, length= 150, command= updatedepth)
+depthslider.place(x=680, y=30)
+depthlabel = Label(root, text='Depth of Field', font=('Arial', 12))
+depthlabel.place(x = 705, y=10)
+
+"VIGNETTE"
+def updatevign(dump):
+    vignlevel = float(vignslider.get())
+    message = f"15, {vignlevel}"
+    send_udp_message(message)
+
+vignslider = Scale(root, from_=0, to=100, orient=HORIZONTAL, length= 150, command= updatevign)
+vignslider.place(x=680, y=90)
+vignlabel = Label(root, text='Vignette', font=('Arial', 12))
+vignlabel.place(x = 718, y=73)
+
+"FOG"
+
+def updatefog(dump):
+    current_value = float(fogslider.get())
+    fog_int = 1 if fog_is_on else 0
+    message = f"16, {fog_int}, {current_value}"
+    send_udp_message(message)
+
+def togglefog():
+    global fog_is_on
+    fog_is_on = not fog_is_on
+    if fog_is_on:
+        fogbtn.config(text='ON', bg='green', fg='white')
+    else:
+        fogbtn.config(text='OFF', bg='red', fg='white')
+    updatefog(1)
+
+fogslider = Scale(root, from_=0.00, to=100, orient=HORIZONTAL, length= 150, command= updatefog)
+fogslider.place(x=680, y=210)
+fogbtn = Button(root, text='OFF', bg='red', fg='white', font=('Arial', 16), command=togglefog, width=10)
+fogbtn.place(x=690, y=170)
+foglabel = Label(root, text='Fog', font=('Arial', 12) )
+foglabel.place(x=730, y=140)
 
 root.mainloop()
